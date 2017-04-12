d37eeab5f86c193da656fb5d8dd57054fd09096c
Morris Brodersen <morris.brodersen.cf@md.de>
2017-04-12 14:27:39 +0200

    fix security merging

--- test/swaggerMerge.js
+++ test/swaggerMerge.js
@@ -4,7 +4,11 @@ var swaggerMin = require( '../lib/swaggerMin' );
 describe( 'swaggerMerge and swaggerMin', function () {
 
   it( 'should be able to merge and minify recursive structures', function () {
-    return swaggerMerge( [ 'test/recursive.yaml',  'test/recursive.yaml' ] ).then( swaggerMin );
+    return swaggerMerge(
+      [ 'test/recursive.yaml',  'test/merge.yaml' ]
+    ).then( swaggerMin ).then( function ( api ) {
+      console.log( JSON.stringify( api, null, '  ' ) );
+    } );
   } );
 
 } );
